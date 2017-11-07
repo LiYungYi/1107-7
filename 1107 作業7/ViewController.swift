d3831676330d125a00274e9c218beb57bb5e6902
泳逸 李 <yongyili@yongyide-MacBook-Air.local>
2017-11-07 19:15:27 +0800

    Initial Commit

---
+++ 1107 作業7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1107 作業7
+//
+//  Created by 泳逸 李 on 2017/11/7.
+//  Copyright © 2017年 yungyili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
