6d1860349701a53dbe68c0f79f58baa3472bf66f
泳逸 李 <yongyili@yongyide-MacBook-Air.local>
2017-11-08 23:10:33 +0800

    color change

--- 1107 作業7/ViewController.swift
+++ 1107 作業7/ViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redText: UILabel!
+    
+    @IBOutlet weak var greenText: UILabel!
+    
+    @IBOutlet weak var blueText: UILabel!
+    
+    @IBOutlet weak var myView: UIView!
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+    
+    var redValue = 255
+    var greenValue = 255
+    var blueValue = 255
+    
+    
+    @IBAction func redSliderChanged(_ sender: UISlider) {
+       redValue = Int(sender.value)
+        redText.text = "R: \(redValue)"
+        changeBackGroundColor()
+    }
+    
+    @IBAction func greenSliderChanged(_ sender: UISlider) {
+        greenValue = Int(sender.value)
+        greenText.text = "G: \(greenValue)"
+        changeBackGroundColor()
+    }
+    
+    @IBAction func blueSliderChanged(_ sender: UISlider) {
+        blueValue = Int(sender.value)
+        blueText.text = "B: \(blueValue)"
+        changeBackGroundColor()
+    }
+    
+    func changeBackGroundColor(){
+        //UIcolor中CFFloat數值須為0~1,需將結果/255
+        myView.backgroundColor = UIColor(red: CGFloat(redValue)/255.0, green: CGFloat(greenValue)/255.0, blue: CGFloat(blueValue)/255.0, alpha: 1)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
